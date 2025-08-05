085c1b2f4918824e67b4e2e5e0b9267e504149f1
mkgg <mk.gregy@gmail.com>
2025-08-05 14:42:08 +0800

    remove company section in footer

--- gag-tracker-frontend/src/components/footer.tsx
+++ gag-tracker-frontend/src/components/footer.tsx
@@ -8,9 +8,6 @@ export function Footer() {
         <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
           <div className="space-y-4">
             <div className="flex items-center gap-2 font-bold">
-              {/* <div className="size-8 rounded-lg bg-gradient-to-br from-primary to-primary/70 flex items-center justify-center text-primary-foreground">
-                G
-              </div> */}
               <Image
                 src="/sugarapple.ico"
                 width={32}
@@ -45,7 +42,7 @@ export function Footer() {
               </Link>
             </div>
           </div>
-          <div className="space-y-4">
+          <div className="space-y-4  md:col-start-3">
             <h4 className="text-sm font-bold">Product</h4>
             <ul className="space-y-2 text-sm">
               <li>
@@ -90,31 +87,7 @@ export function Footer() {
               </li>
             </ul>
           </div>
-          <div className="space-y-4">
-            <h4 className="text-sm font-bold">Company</h4>
-            <ul className="space-y-2 text-sm">
-              <li>
-                <Link href="#" className="text-muted-foreground hover:text-foreground transition-colors">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="text-muted-foreground hover:text-foreground transition-colors">
-                  Careers
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="text-muted-foreground hover:text-foreground transition-colors">
-                  Privacy Policy
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="text-muted-foreground hover:text-foreground transition-colors">
-                  Terms of Service
-                </Link>
-              </li>
-            </ul>
-          </div>
+          
         </div>
         <div className="flex flex-col gap-4 sm:flex-row justify-between items-center border-t border-border/40 pt-8">
           <p className="text-xs text-muted-foreground">
